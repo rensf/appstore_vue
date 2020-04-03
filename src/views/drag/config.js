b0afa410e93d61c5f694b02ca83171123242617f
rensf <2636618553@qq.com>
2020-04-03 18:27:54 +0800

    做一个拖拽组件

---
+++ src/views/drag/config.js
@@ -0,0 +1,48 @@
+export const inputComponents = [
+    {
+        label: '单行文本',
+        tag: 'i-input',
+        tagIcon: '',
+        placeholder: '请输入',
+        defaultValue: undefined,
+    },
+    {
+        label: '多行文本',
+        tag: 'i-input',
+        type: 'textarea',
+        placeholder: '请输入',
+        defaultValue: undefined,
+    },
+    {
+        label: '密码',
+        tag: 'i-input',
+        type: 'password',
+        placeholder: '请输入',
+        defaultValue: undefined,
+    }
+];
+
+export const selectComponents = [
+    {
+        label: '下拉选择',
+        tag: 'i-select',
+        placeholder: '请输入',
+        defaultValue: undefined,
+    },
+    {
+        label: '单选框',
+        tag: 'i-radio'
+    },
+    {
+        label: '复选框',
+        tag: 'i-checkbox'
+    },
+    {
+        label: '时间选择',
+        tag: 'i-date-picker'
+    },
+    {
+        label: '文件上传',
+        tag: 'i-upload'
+    }
+];
--- src/views/drag/drag.vue
+++ src/views/drag/drag.vue
@@ -3,22 +3,28 @@
         <Card title="自定义表单（可拖拽）">
             <div style="display: flex;">
                 <div style="width: 300px;">
-                    <ul id="test1" class="draggable-list">
-                        <li v-for="(item, index) in testList" :key="index">{{item.content}}</li>
+                    <ul ref="test1" class="draggable-list">
+                        <li v-for="(item, index) in testList" :key="index">
+                            {{item.content}}
+                        </li>
+<!--                        <li>-->
+<!--                            <Radio>男</Radio>-->
+<!--                        </li>-->
                     </ul>
                 </div>
                 <Divider type="vertical"/>
                 <div style="width: 100%;">
-                    <ul id="test2" class="select-list">
-
-                    </ul>
+                    <ul ref="test2" class="select-list"></ul>
                 </div>
                 <Divider type="vertical"/>
                 <div style="width: 300px;">
-
+                    <Form>
+                        <FormItem v-for="(item, index) in actionForm.items" :key="index" :prop="item.prop">
+                            <Input v-model="item.value"/>
+                        </FormItem>
+                    </Form>
                 </div>
             </div>
-
         </Card>
     </div>
 </template>
@@ -43,11 +49,17 @@
                     {
                         content: '下拉框'
                     },
-                ]
+                ],
+                selectList: [],
+                actionForm: {
+                    items: []
+                },
+                aaa: '1111111'
             };
         },
         mounted () {
-            let test1 = document.getElementById('test1');
+            let that = this;
+            let test1 = this.$refs.test1;
             Sortable.create(test1, {
                 group: {
                     name: 'shared',
@@ -57,11 +69,28 @@
                 animation: 120,
                 sort: false,
                 onRemove (event) {
-                    event.item.innerHTML = '<p>hhhhhh</p>';
-                    console.log(event);
+                    switch (event.clone.innerText) {
+                        case '文本框': {
+                            event.item.innerHTML.template = '<Input/>';
+                        }
+                        case '单选框': {
+                            event.item.innerHTML = '<Radio>男</Radio>'
+                        }
+                    }
+                    console.log(event.item.innerHTML);
+                    // let obj = {
+                    //     title: event.clone.innerText,
+                    //     placeholder: ''
+                    // };
+                    // Object.entries(obj).forEach(item => {
+                    //     console.log(item);
+                    //
+                    // });
+                    // that.selectList.splice(event.newIndex, 0, obj);
+                    // console.log(that.selectList);
                 }
             });
-            let test2 = document.getElementById('test2');
+            let test2 = this.$refs.test2;
             Sortable.create(test2, {
                 group: 'shared',
                 animation: 120
